8ac622e21f3c6c02f9b175c3caa8c9364a287872
Lieety <lieety10.07@gmail.com>
2025-06-09 22:31:59 +0200

    s'havia colat una 's'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Perfil from './pages/Perfil';
 
-function App() {s
+function App() {
   return (
     <>
       <Navbar />
