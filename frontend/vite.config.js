9fc68440ed0ae6f38547a875e7b545be981a3e58
Lieety <lieety10.07@gmail.com>
2025-06-11 18:20:08 +0200

    fuck no va

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
-  },
-});
+    historyApiFallback: true // 👈 Per evitar 404s en rutes internes
+  }
+})
