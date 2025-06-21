fa997541e32277a9d2bf3fc1910bc0cea8757b9e
Guillermo Ballarin <guilleballarin73@gmail.com>
2025-06-21 23:15:47 +0200

    tot canviat

--- backend/routes/flashcardRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const router = express.Router();
-router.post('/generate', (req, res) => {
-    res.json({ flashcard: "Flashcard generada (fake data)" });
-});
-export default router;
